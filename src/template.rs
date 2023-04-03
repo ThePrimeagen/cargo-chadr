4c6a6e0fb8afac8fc2145995a7a93533e0b08702
mpaulson <the.primeagen@gmail.com>
2023-04-03 10:48:53 -0600

    feat: suckt it again melkey

--- src/template.rs
+++ src/template.rs
@@ -58,17 +58,6 @@ move "anotherroute"                to routing-destiny(4).
 
 */
 
-move 3 to nroutes.
-
-move "/"                           to routing-pattern(1).
-move "indexweb"                    to routing-destiny(1).
-
-move "/showsum/%value1/%value2"    to routing-pattern(2).
-move "showsum"                     to routing-destiny(2).
-
-move "/showname/%value"            to routing-pattern(3).
-move "showname"                    to routing-destiny(3).
-
 pub const ROUTES: &'static str = r#"
 move __ROUTE_COUNT__ to nroutes.
 
